c12836213ddfa42a49a62fbc17f3c7ca2428780f
vivian1008 <56061904+vivian1008@users.noreply.github.com>
2021-03-29 19:52:42 +0800

    Add files via upload

--- 108021016 林雨璇 5-3triangle.py
+++ 108021016 林雨璇 5-3triangle.py
@@ -7,9 +7,11 @@ Created on Fri Mar 26 18:56:46 2021
 
 # 108021016 林雨璇 5-3triangle.py
 
-n = int(input('三角形的高度?'))
-for i in range(1,n+1):
-    for j in range(1,i*2): 
+n = int(input('三角形的高度?')) #讓使用者輸入三角形的高度
+for i in range(1,n+1): #換行的迴圈
+    for j in range (n,i,-1): #輸出空格的迴圈
         print(' ',end='')
+    for k in range(1,i*2):  #輸出星號的迴圈
         print('*',end='')
     print()
+    
--- 108021016 林雨璇 least_n_fac.py
+++ 108021016 林雨璇 least_n_fac.py
@@ -7,13 +7,12 @@ Created on Sun Mar 28 13:35:13 2021
 
 # 108021016 林雨璇 least_n_fac.py
 
-M = int(input('輸入一個值'))
-f = 2
-fac = 1
-while fac < M:
-    fac = fac*f
-    f+=1
-    print(fac/f)
-
-    
+M = int(input('輸入一個值')) #讓使用者輸入一個數
+f = 1
+N = 1
+while N < M: #當N!沒超過M
+    N = N*f #N!就繼續往上乘
+    f+=1  #f遞增
+    if N > M: #如果N!大於M
+     print(N) #輸出N!
     
