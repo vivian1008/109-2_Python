ad09b6077d1194a28b93d27682fc59002e4deaa0
vivian1008 <56061904+vivian1008@users.noreply.github.com>
2021-04-09 15:54:44 +0800

    Add files via upload

---
+++ 108021016 林雨璇 6-1-PrimeTest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#108021016 林雨璇 6-1-PrimeTest.py
+import math
+
+def divisiible(a,b):
+   while b < a:
+       if(a % b == 0):
+           print('不是質數')
+           break
+       b+=1
+   else:
+       print('是質數')
+            
+n = int(input('輸入一個數'))
+divisiible(n,n/2)    
+
